ccbac4b565c45152d12d82188e5d2e82ac66d16e
harry <316361680@qq.com>
2021-01-04 17:08:01 +0800

    1

--- auto.py
+++ auto.py
@@ -16,25 +16,25 @@ plt.grid(ls="--")
 plt.show()
 
 
-def update_points(num):
-    '''
-    更新数据点
-    '''
-    point_ani.set_data(x[num], y[num])
-    return point_ani,
+# def update_points(num):
+#     '''
+#     更新数据点
+#     '''
+#     point_ani.set_data(x[num], y[num])
+#     return point_ani,
  
-x = np.linspace(0, 2*np.pi, 100)
-y = np.sin(x)
+# x = np.linspace(0, 2*np.pi, 100)
+# y = np.sin(x)
  
-fig = plt.figure(tight_layout=True)
-plt.plot(x,y)
-point_ani, = plt.plot(x[0], y[0], "ro")
-plt.grid(ls="--")
-# 开始制作动画
-ani = animation.FuncAnimation(fig, update_points, np.arange(0, 100), interval=100, blit=True)
+# fig = plt.figure(tight_layout=True)
+# plt.plot(x,y)
+# point_ani, = plt.plot(x[0], y[0], "ro")
+# plt.grid(ls="--")
+# # 开始制作动画
+# ani = animation.FuncAnimation(fig, update_points, np.arange(0, 100), interval=100, blit=True)
  
-# ani.save('sin_test2.gif', writer='imagemagick', fps=10)
-plt.show()
+# # ani.save('sin_test2.gif', writer='imagemagick', fps=10)
+# plt.show()
 
 
 def update_points(num):
